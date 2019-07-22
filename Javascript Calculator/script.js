ce6b93a3a570a9a0797c0d40f5683594a69cc2f0
Brian Stevenson <52355699+redmoonX@users.noreply.github.com>
2019-07-21 22:34:00 -0500

    Javascript Calculator Project
    personal project

---
+++ Javascript Calculator/script.js
@@ -0,0 +1,90 @@
+// VARIABLES: N1 N2 MODE SOLUTION 
+// FUNCTIONS: INPUT1 INPUT2 ARITHMETIC(=) CLEAR MEMCLEAR MEMSTORE MEMRECALL
+
+// RECEIVE NUMBER INPUT1 => N1
+
+// check n1, if empty launch input1 and display else push to n2
+
+// DISPLAY N1 IN WINDOW
+
+
+
+// RECEIVE ARITHMETIC MODE => MODE
+
+
+
+// RECEIVE NUMBER INPUT2 => N2
+
+
+
+// DISPLAY N2 IN WINDOW
+
+
+
+// PERFORM ARITHMETIC (=)
+
+
+
+// DISPLAY SOLUTION IN WINDOW
+
+
+
+// CLEAR FUNCTION
+
+
+
+// MEMORY CLEAR FUNCTION 
+
+
+
+// MEMORY STORE FUNCTION
+
+
+
+// MEMORY RECALL FUNCTION
+var n1 = [];
+var n2 = [];
+var mode = [];
+var solution = [];
+// if (mode === []) {
+function inputOne() {
+  n1.push(1);
+  document.getElementById("output").innerHTML = n1.join('');
+}
+function inputTwo() {
+  n1.push(2);
+  document.getElementById("output").innerHTML = n1.join('');
+}
+function inputThree() {
+  n1.push(3);
+  document.getElementById("output").innerHTML = n1.join('');
+}
+function inputFour() {
+  n1.push(4);
+  document.getElementById("output").innerHTML = n1.join('');
+}
+function inputFive() {
+  n1.push(5);
+  document.getElementById("output").innerHTML = n1.join('');
+}
+function inputSix() {
+  n1.push(6);
+  document.getElementById("output").innerHTML = n1.join('');
+}
+function inputSeven() {
+  n1.push(7);
+  document.getElementById("output").innerHTML = n1.join('');
+}
+function inputEight() {
+  n1.push(8);
+  document.getElementById("output").innerHTML = n1.join('');
+}
+function inputNine() {
+  n1.push(9);
+  document.getElementById("output").innerHTML = n1.join('');
+}
+function inputZero() {
+  n1.push(0);
+  document.getElementById("output").innerHTML = n1.join('');
+}
+// } else {
