4105a5ac2881bbe0475c9ec070ee505705a184cf
redmoonX <52355699+redmoonX@users.noreply.github.com>
2019-08-25 17:31:31 -0500

    Starwars React API
    rolled out prev/next functionality for all components

--- src/App.js
+++ src/App.js
@@ -12,7 +12,11 @@ class App extends Component {
       this.state = { 
         robots: [],
         allchars: [],
-        charindex: 1,
+        charindex: 0,
+        worldindex: 0,
+        shipindex: 0,
+        vehicleindex: 0,
+        filmindex: 0,
         worlds: [],
         cworlds: [],
         ships: [],
@@ -27,80 +31,72 @@ class App extends Component {
       }
   }
 
-  componentDidMount() {
-    fetch('https://swapi.co/api/people/6/')
-    .then(response => response.json())
-    .then(users => this.setState( { robots: users }));
-    fetch('https://swapi.co/api/planets/1/')
-    .then(response => response.json())
-    .then(users => this.setState( { worlds: users }));
-    fetch('https://swapi.co/api/starships/2/')
-    .then(response => response.json())
-    .then(users => this.setState( { ships: users }));
-    fetch('https://swapi.co/api/vehicles/4/')
-    .then(response => response.json())
-    .then(users => this.setState( { vehix: users }));
-    fetch('https://swapi.co/api/films/1/')
-    .then(response => response.json())
-    .then(users => this.setState( { films: users }));
-    }
+  // componentDidMount() {
+  //   // fetch('https://swapi.co/api/people/6/')
+  //   // .then(response => response.json())
+  //   // .then(users => this.setState( { robots: users }));
+  //   // fetch('https://swapi.co/api/planets/1/')
+  //   // .then(response => response.json())
+  //   // .then(users => this.setState( { worlds: users }));
+  //   fetch('https://swapi.co/api/starships/2/')
+  //   .then(response => response.json())
+  //   .then(users => this.setState( { ships: users }));
+  //   fetch('https://swapi.co/api/vehicles/4/')
+  //   .then(response => response.json())
+  //   .then(users => this.setState( { vehix: users }));
+  //   fetch('https://swapi.co/api/films/1/')
+  //   .then(response => response.json())
+  //   .then(users => this.setState( { films: users }));
+  //   }
 
 render() {
-  // Characters
 
-  // console.log(this.state);
-  // console.log(this.state.allchards.results[0].name);
-  // const giveitDATA = this.state;
-  // const testy = JSON.stringify(giveitDATA);
-  // const test1 = 
-  // var test2 =  
-  //   fetch('https://swapi.co/api/people/')
-  //   .then(response => console.log(response.json()))
-      // JSON.parse(response.json()))
-    // .then(console.log())
-    // .then(data => test2 = data)
 
-    
-    // .then(test1.results => 
     const self = this;
     async function apiGetAll () {
-      // var charindx = this.state.charindex
       const resp = await fetch('https://swapi.co/api/people/')
       const data = await resp.json()
       const detail = await data.results[self.state.charindex]
       const statehood = await self.setState( { allchars: detail } )
-      // console.log(data)
-      return detail }
-    // } catch (err) {
-    //   console.log(err)
-    // }
-
-    ;
+      return detail };
   apiGetAll();
-// apiGetAll();
-// console.log(this.state.allchars);
-    // console.log(this.state.charindex);
-  
-  // var test1 = test2;
-  // console.log(test2);
-    // .then(() => test2.results[this.state.charindex];
-    // .then(() => )
-    // .then(() => console.log(test2.results[this.state.charindex]));
-  // console.log(test2.results[this.state.charindex]);
-  // console.log(test2);
+
+    async function apiGetAll1 () {
+      const resp = await fetch('https://swapi.co/api/planets/')
+      const data = await resp.json()
+      const detail = await data.results[self.state.worldindex]
+      const statehood = await self.setState( { worlds: detail } )
+      return detail };
+  apiGetAll1();
+
+      async function apiGetAll2 () {
+      const resp = await fetch('https://swapi.co/api/starships/')
+      const data = await resp.json()
+      const detail = await data.results[self.state.shipindex]
+      const statehood = await self.setState( { ships: detail } )
+      return detail };
+  apiGetAll2();
+
+      async function apiGetAll3 () {
+      const resp = await fetch('https://swapi.co/api/vehicles/')
+      const data = await resp.json()
+      const detail = await data.results[self.state.vehicleindex]
+      const statehood = await self.setState( { vehix: detail } )
+      return detail };
+  apiGetAll3();
+
+      async function apiGetAll4 () {
+      const resp = await fetch('https://swapi.co/api/films/')
+      const data = await resp.json()
+      const detail = await data.results[self.state.filmindex]
+      const statehood = await self.setState( { films: detail } )
+      return detail };
+  apiGetAll4();
+
+// Characters
   const giveitDATA = this.state.allchars;
-  // console.log(giveitDATA);
-  // console.log(giveitDATA);
-  // console.log(test1);
-    // .then(users => this.setState( { allchars: [users] }));
-  // console.log(testy);
-  // console.log(giveitDATA);
-  // const { prenameJob } = giveitDATA.allchars;
-  // const namejobtest = prenameJob;
-  // console.log(namejobtest);
+
   const nameJob = giveitDATA.name;
-  // console.log(prenameJob);
-  // const nameJob = giveitDATA.robots.name;
   const genderJob = giveitDATA.gender;
   const heightJob = giveitDATA.height;
   const massJob = giveitDATA.mass;
@@ -108,26 +104,20 @@ render() {
   const homewFetchJob = () => {
     fetch(homewJob)
     .then(response => response.json())
-    .then(world => this.setState({ worlds: world }));
+    .then(world => this.setState({ cworlds: world }));
   };
   homewFetchJob();
+  const homewcValueJob = this.state.cworlds.name;
   const homewValueJob = this.state.worlds.name;
- 
-   // Homeworlds
+  
+  // Homeworlds
   const giveitDATA1 = this.state;
   const nameJob1 = giveitDATA1.worlds.name;
   const genderJob1 = giveitDATA1.worlds.climate;
   const heightJob1 = giveitDATA1.worlds.terrain;
   const massJob1 = giveitDATA1.worlds.population;
   const homewJob1 = giveitDATA1.worlds.residents;
-  // const homewFetchJob1 = giveitDATA1.worlds.residents.map(url => fetch(url)
-  //   .then(response => response.json())
-  //   .then(value => this.setState({ residents: value })));
-  
-  // homewFetchJob1();
-  // const homewValueJob1 = this.state.residents.name;
-  // console.log(this.state);
-  // console.log(homewJob1);
+
   // Starships
   const giveitDATA2 = this.state;
   const nameJob2 = giveitDATA2.ships.name;
@@ -157,14 +147,18 @@ render() {
   const viewToggle = viewToggleCalc.cview;
   const viewToggleCalc1 = this.state;
   const viewToggle1 = viewToggleCalc1.wview;
-    const viewToggleCalc2 = this.state;
+  const viewToggleCalc2 = this.state;
   const viewToggle2 = viewToggleCalc2.sview;
-    const viewToggleCalc3 = this.state;
+  const viewToggleCalc3 = this.state;
   const viewToggle3 = viewToggleCalc3.vview;
-    const viewToggleCalc4 = this.state;
+  const viewToggleCalc4 = this.state;
   const viewToggle4 = viewToggleCalc4.fview;
+  const cbutton = this.state.charindex;
+  const wbutton = this.state.worldindex;
+  const sbutton = this.state.shipindex;
+  const vbutton = this.state.vehicleindex;
+  const fbutton = this.state.filmindex;
     
-
     return ( 
         <div className='tc'>
           <h1 className='f1'>StarWars Explorer</h1>
@@ -177,25 +171,83 @@ render() {
           </div>
             <div className={viewToggle}>
               <h2>Characters</h2>
-                <Characters charName={nameJob} charHeight={heightJob} charMass={massJob} charHome={homewValueJob} charGender={genderJob}>Characters</Characters>
+                <Characters charName={nameJob} charHeight={heightJob} charMass={massJob} charHome={homewcValueJob} charGender={genderJob}>Characters</Characters>
             </div>
-            <button className={viewToggle} onClick={() => this.setState({ charindex: this.state.charindex - 1 })}> Prev </button>
-            <button className={viewToggle} onClick={() => this.setState({ charindex: this.state.charindex + 1 })}> Next </button>
+            <div className="CharCounter">
+            { cbutton > 0 ? (<button className={viewToggle} onClick={() => this.setState({ charindex: this.state.charindex - 1 })}> Prev </button>)
+            : 
+            ( <button className={viewToggle}> Prev </button>)
+            }
+           
+             
+            { cbutton < 9 ? (<button className={viewToggle} onClick={() => this.setState({ charindex: this.state.charindex + 1 })}> Next </button>)
+            : 
+            ( <button className={viewToggle}> Next </button>)
+            }
+            </div>
             <div className={viewToggle1}>
               <h2>Homeworlds</h2>
                 <Homeworlds charName={nameJob1} charHeight={heightJob1} charMass={massJob1} charHome={homewValueJob} charGender={genderJob1}>Characters</Homeworlds>
+            </div>
+            <div className="HomeworldsCounter">
+            { wbutton > 0 ? (<button className={viewToggle1} onClick={() => this.setState({ worldindex: this.state.worldindex - 1 })}> Prev </button>)
+            : 
+            ( <button className={viewToggle1}> Prev </button>)
+            }
+           
+             
+            { wbutton < 9 ? (<button className={viewToggle1} onClick={() => this.setState({ worldindex: this.state.worldindex + 1 })}> Next </button>)
+            : 
+            ( <button className={viewToggle1}> Next </button>)
+            }
             </div>
             <div className={viewToggle2}>
               <h2>Starships</h2>
                 <Starships charName={nameJob2} charHeight={heightJob2} charMass={massJob2} charHome={homewValueJob} charGender={genderJob2}>Characters</Starships>
             </div>
+            <div className="StarshipsCounter">
+            { sbutton > 0 ? (<button className={viewToggle2} onClick={() => this.setState({ shipindex: this.state.shipindex - 1 })}> Prev </button>)
+            : 
+            ( <button className={viewToggle2}> Prev </button>)
+            }
+           
+             
+            { sbutton < 9 ? (<button className={viewToggle2} onClick={() => this.setState({ shipindex: this.state.shipindex + 1 })}> Next </button>)
+            : 
+            ( <button className={viewToggle2}> Next </button>)
+            }
+            </div>
             <div className={viewToggle3}>
               <h2>Vehicles</h2>
                 <Vehicles charName={nameJob3} charHeight={heightJob3} charMass={massJob3} charHome={homewValueJob} charGender={genderJob3}>Characters</Vehicles>
             </div>
+            <div className="VehiclesCounter">
+            { vbutton > 0 ? (<button className={viewToggle3} onClick={() => this.setState({ vehicleindex: this.state.vehicleindex - 1 })}> Prev </button>)
+            : 
+            ( <button className={viewToggle3}> Prev </button>)
+            }
+           
+             
+            { vbutton < 9 ? (<button className={viewToggle3} onClick={() => this.setState({ vehicleindex: this.state.vehicleindex + 1 })}> Next </button>)
+            : 
+            ( <button className={viewToggle3}> Next </button>)
+            }
+            </div>
             <div className={viewToggle4}>
               <h2>Films</h2>
                 <Films charName={nameJob4} charHeight={heightJob4} charMass={massJob4} charHome={homewValueJob} charGender={genderJob4}>Characters</Films>
+            </div>
+            <div className="FilmsCounter">
+            { fbutton > 0 ? (<button className={viewToggle4} onClick={() => this.setState({ filmindex: this.state.filmindex - 1 })}> Prev </button>)
+            : 
+            ( <button className={viewToggle4}> Prev </button>)
+            }
+           
+             
+            { fbutton < 6 ? (<button className={viewToggle4} onClick={() => this.setState({ filmindex: this.state.filmindex + 1 })}> Next </button>)
+            : 
+            ( <button className={viewToggle4}> Next </button>)
+            }
             </div>
         </div>
             );
