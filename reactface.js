dc9b1ed570d5715552f1504a931de8a0ef99c853
redmoonX <52355699+redmoonX@users.noreply.github.com>
2019-09-06 21:12:06 -0500

    reactface final project

---
+++ reactface.js
@@ -0,0 +1,16 @@
+import 'React', { Component } from 'react';
+
+class reactface extends Component {
+constructor() {
+super() 
+	this.state: {
+		login: false,
+		picupload: false,
+		rank: 0
+
+
+	};
+
+}
+
+}
