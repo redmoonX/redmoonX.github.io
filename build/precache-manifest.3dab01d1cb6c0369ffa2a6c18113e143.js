a8481b8f2cdeae7eebaa0e8554035f03547f7a9d
redmoonX <52355699+redmoonX@users.noreply.github.com>
2019-09-07 19:49:46 -0500

    react sw api hosting

---
+++ build/precache-manifest.3dab01d1cb6c0369ffa2a6c18113e143.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0755ab0f51eacd7e83913e721dbad763",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "e3dfd9d23f5865a57173",
+    "url": "/build/static/css/main.318e3027.chunk.css"
+  },
+  {
+    "revision": "d6596e493dbf4a19e6a3",
+    "url": "/build/static/js/2.7b2a79c6.chunk.js"
+  },
+  {
+    "revision": "e3dfd9d23f5865a57173",
+    "url": "/build/static/js/main.3a141c0a.chunk.js"
+  },
+  {
+    "revision": "f12c87fb5951217cf5de",
+    "url": "/build/static/js/runtime~main.5543331f.js"
+  }
+]);
